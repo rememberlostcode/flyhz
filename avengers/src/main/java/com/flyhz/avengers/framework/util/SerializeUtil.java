5178a7d028a5cd920cfd2cad4bc2b8664acbd95b
fuwb1027 <syqf1027@163.com>
2014-06-23 07:53:01 +0800

    new file:   src/main/java/com/flyhz/avengers/framework/util/SerializeUtil.java

---
+++ avengers/src/main/java/com/flyhz/avengers/framework/util/SerializeUtil.java
@@ -0,0 +1,55 @@
+
+package com.flyhz.avengers.framework.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ * 对象序列化和反序列化工具类
+ * 
+ * @author fuwb
+ */
+public class SerializeUtil {
+	/**
+	 * 序列化
+	 * 
+	 * @param object
+	 * @return byte[]
+	 */
+	public static byte[] serialize(Object object) {
+		ObjectOutputStream oos = null;
+		ByteArrayOutputStream baos = null;
+		try {
+			// 序列化
+			baos = new ByteArrayOutputStream();
+			oos = new ObjectOutputStream(baos);
+			oos.writeObject(object);
+			byte[] bytes = baos.toByteArray();
+			return bytes;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 * 反序列化
+	 * 
+	 * @param bytes
+	 * @return byte[]
+	 */
+	public static Object unserialize(byte[] bytes) {
+		ByteArrayInputStream bais = null;
+		try {
+			// 反序列化
+			bais = new ByteArrayInputStream(bytes);
+			ObjectInputStream ois = new ObjectInputStream(bais);
+			return ois.readObject();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
