61bf96778c36ba4c5eea2ac25a5421befdc9fbb7
zhangb <bbbbb1202@163.com>
2014-08-21 21:32:21 +0800

    modified:   src/main/java/com/flyhz/shop/build/solr/SolrDataImpl.java

--- smile/src/main/java/com/flyhz/shop/build/solr/SolrDataImpl.java
+++ smile/src/main/java/com/flyhz/shop/build/solr/SolrDataImpl.java
@@ -186,7 +186,7 @@ public class SolrDataImpl implements SolrData {
 				discountDto = new DiscountDto();
 				discountDto.setId(list.get(i).getId());;
 				discountDto.setDiscount(list.get(i).getDiscount());
-				discountDto.setDp(productBuildDto.getPp().multiply(new BigDecimal(list.get(i).getDiscount())));
+				discountDto.setDp(productBuildDto.getPp().multiply(new BigDecimal(list.get(i).getDiscount() / 10.0)));
 				disList.add(discountDto);
 			}
 			doc.addField("discounts", JSONUtil.getEntity2Json(disList));// 折扣
